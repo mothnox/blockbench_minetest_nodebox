c7d7bb9d26d37dae20c14a9b4999f1af6c8396c4
mothnox <>
2022-11-05 16:01:06 -0500

    Initial commit

---
+++ minetest_nodebox.js
@@ -0,0 +1,9 @@
+Plugin.register('minetest_nodebox', {
+	title: 'Minetest Nodebox',
+	author: 'mothnox',
+	icon: 'placeholder',
+	description: 'Adds a format for creating nodeboxes for Minetest',
+	version: '0.1.0',
+	variant: 'both',
+	onload(){},
+});
