144d04a081fd8a14c8d73c3d4e88c2c93cf5d702
mothnox <>
2022-11-05 21:40:39 -0500

    Add license information

--- minetest_nodebox.js
+++ minetest_nodebox.js
@@ -1,3 +1,15 @@
+/*
+     This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>. 
+*/
+
+/*
+Some code of this plugin was copied and modified from java_block.js of blockbench (https://github.com/JannisX11/blockbench), on Nov. 5, 2022.
+*/
+
 Plugin.register('minetest_nodebox', {
 	title: 'Minetest Nodebox',
 	author: 'mothnox',
